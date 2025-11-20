0d64324e305ecc4c8091903a65b7ac132f8eda5c
Carole Trem <trem.carole@gmail.com>
2025-11-20 16:55:41 +0100

    first commit

---
+++ ZakFitFront/ZakFitFrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFitFrontApp.swift
+//  ZakFitFront
+//
+//  Created by caroletm on 20/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ZakFitFrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
